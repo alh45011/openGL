cbe52ce847c018387a93bec4db7347e682876844
alh45011 <alh45011@gmail.com>
2024-11-10 20:31:27 +0000

    2

--- GL_experiment/GL_experiment/shader.cpp
+++ GL_experiment/GL_experiment/shader.cpp
@@ -68,6 +68,11 @@ void Shader::setBool(const std::string& name, bool value) const
 {
     glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
 }
+
+void Shader::setMat4(const std::string& name, const glm::mat4& mat) const
+{
+    glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, glm::value_ptr(mat));
+}
 // ------------------------------------------------------------------------
 void Shader::setInt(const std::string& name, int value) const
 {
@@ -78,7 +83,6 @@ void Shader::setFloat(const std::string& name, float value) const
 {
     glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
 }
-
 
 void Shader::checkCompileErrors(unsigned int shader, std::string type)
 {
--- GL_experiment/GL_experiment/shader.hpp
+++ GL_experiment/GL_experiment/shader.hpp
@@ -3,11 +3,14 @@
 
 #include <glad/glad.h> // include glad to get all the required OpenGL headers
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #include <string>
 #include <fstream>
 #include <sstream>
 #include <iostream>
-
 
 class Shader
 {
@@ -23,6 +26,8 @@ public:
     void setBool(const std::string& name, bool value) const;
     void setInt(const std::string& name, int value) const;
     void setFloat(const std::string& name, float value) const;
+
+    void setMat4(const std::string& name, const glm::mat4& mat) const;
 
 private:
     // utility function for checking shader compilation/linking errors.
